93381ad9afd3c26f652b5310a222b8b8eb2e3d47
Ashwini Shenoy B <146318321+ShenoyAsh@users.noreply.github.com>
2026-08-01 15:22:40 +0530

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.cpp
@@ -0,0 +1,70 @@
+
+class Solution {
+public:
+    vector<vector<char>> board;
+    string word;
+    int rows, cols;
+
+    bool buildWord(int i, int j, int idx) {
+
+        // 1. Out of bounds
+        if (i < 0 || j < 0 || i >= rows || j >= cols)
+            return false;
+
+        // 2. Character doesn't match
+        if (board[i][j] != word[idx])
+            return false;
+
+        // 3. Entire word matched
+        if (idx == word.length() - 1)
+            return true;
+
+        // 4. Mark current cell as visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // 5. Explore all four directions
+        bool found =
+            buildWord(i + 1, j, idx + 1) ||   // Down
+            buildWord(i - 1, j, idx + 1) ||   // Up
+            buildWord(i, j + 1, idx + 1) ||   // Right
+            buildWord(i, j - 1, idx + 1);     // Left
+
+        // 6. Restore original character (Backtracking)
+        board[i][j] = temp;
+
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+
+        this->board = board;
+        this->word = word;
+
+        rows = board.size();
+        cols = board[0].size();
+
+        // Try every cell as a starting point
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (this->board[i][j] == word[0]) {
+                    if (buildWord(i, j, 0))
+                        return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<string> ans;
+
+for (string word : words) {
+    if (exist(board, word)) {
+        ans.push_back(word);
+    }
+}return ans;
+    }
+};
